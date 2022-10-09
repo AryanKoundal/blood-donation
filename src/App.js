861f3a7f08859c690b305c251c1de5d6105fec0c
ARYAN K <aryankoundal474@gmail.com>
2022-10-09 19:33:08 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './pages/Navbar';
+import Home from "./pages/Home";
+// import Header from "./pages/Header";
+import { Admin } from "./pages/Admin";
+import { BloodBank } from "./pages/BloodBank";
+import { Donor } from "./pages/Donor";
+
+import { Patient } from "./pages/Patient";
+import { BrowserRouter as Router, Routes, Route }
+  from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>hello</h1>
+      <Home />
+      <Navbar />
+      {/* <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/admin' element={<Admin />} />
+          <Route exact path='/bloodbank' element={<BloodBank />} />
+          <Route exact path='/donor' element={<Donor />} />
+          <Route exact path='/patient' element={<Patient />} />
+        </Routes>
+      </Router> */}
+    </>
   );
 }
 
